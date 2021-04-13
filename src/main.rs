69d4f6da1650f609bfeb6bb657ff4e7e22930ac4
ChenKS12138 <chen749923710@gmail.com>
2021-04-13 22:40:12 +0800

    feat: error handle

--- src/main.rs
+++ src/main.rs
@@ -16,32 +16,42 @@ async fn main() -> Result<(), Error> {
 
     let mut stream = api.stream();
     while let Some(update) = stream.next().await {
-        let update = update?;
-        if let UpdateKind::Message(message) = update.kind {
-            match message.kind {
-                MessageKind::Text { ref data, .. } => {
-                    let command = data.as_str();
-                    let command = reg.replace_all(&command, "").to_owned().to_string();
-                    match command.as_str() {
-                        COMMAND_KS => {
-                            api.send(types::requests::SendVideo::new(&message.chat, &ks_gif))
-                                .await?;
+        if update.is_ok() {
+            let update = update.ok();
+            if let Some(update) = update {
+                if let UpdateKind::Message(message) = update.kind {
+                    match message.kind {
+                        MessageKind::Text { ref data, .. } => {
+                            let command = data.as_str();
+                            let command = reg.replace_all(&command, "").to_owned().to_string();
+                            match command.as_str() {
+                                COMMAND_KS => {
+                                    api.send(types::requests::SendVideo::new(
+                                        &message.chat,
+                                        &ks_gif,
+                                    ))
+                                    .await?;
+                                }
+                                COMMAND_GITHUB => {
+                                    api.send(types::requests::SendMessage::new(
+                                        &message.chat,
+                                        "See https://github.com/ChenKS12138/tg-ks-bot",
+                                    ))
+                                    .await?;
+                                }
+                                _ => {
+                                    api.send(types::requests::SendMessage::new(
+                                        &message.chat,
+                                        "Say /ks",
+                                    ))
+                                    .await?;
+                                }
+                            }
                         }
-                        COMMAND_GITHUB => {
-                            api.send(types::requests::SendMessage::new(
-                                &message.chat,
-                                "See https://github.com/ChenKS12138/tg-ks-bot",
-                            ))
-                            .await?;
-                        }
-                        _ => {
-                            api.send(types::requests::SendMessage::new(&message.chat, "Say /ks"))
-                                .await?;
-                        }
+                        _ => (),
                     }
                 }
-                _ => (),
-            };
+            }
         }
     }
     Ok(())
